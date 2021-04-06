f24e27081609a8af5f2291884aace1374c121586
Harrison Chen <harryandc@gmail.com>
2021-04-06 14:22:00 -0700

    swap to darker solarized text

--- wmpc_pager.php
+++ wmpc_pager.php
@@ -25,7 +25,7 @@
 </head>
 
 <!-- Full site background color HEX -->
-<body bgcolor="#EDE0BB" text="#073642">
+<body bgcolor="#EDE0BB" text="#596C72">
 
   <table width="1000" border="0" cellpadding="3" cellspacing="1">
     <!-- 
