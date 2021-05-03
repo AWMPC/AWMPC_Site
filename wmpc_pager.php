fe15162cbcfd5e8e083667c58b2c7243a2192743
Harrison Chen <harryandc@gmail.com>
2021-05-02 22:59:27 -0700

    try first of triadic pair

--- wmpc_pager.php
+++ wmpc_pager.php
@@ -4,7 +4,7 @@
   <title>World Mission Prayer Center - 信望愛禱告中心 </title>
   <meta name="keywords" content="awmpc,wmpc,awmpc.org,World Mission Prayer Center" />
   <meta http-equiv="content-type" content="text/html;charset=utf-8" />
-  <meta name="theme-color" content="#e3d5aa">
+  <meta name="theme-color" content="#e3aac6">
   <style>
     body {
       text-align: center;
